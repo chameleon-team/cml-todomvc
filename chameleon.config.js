4249dc1e82a9034e20bc542d931df3bd50440dcf
JiM-W <550116990@qq.com>
2019-07-17 18:35:05 +0800

    cml-todomvc

--- chameleon.config.js
+++ chameleon.config.js
@@ -3,11 +3,33 @@
 const publicPath = 'https://beatles-chameleon.github.io/cml-todomvc/dist';
 // 设置api请求前缀
 const apiPrefix = 'https://api.chameleon.com';
-
+const path = require('path');
 cml.config.merge({
   templateLang: 'cml',
   templateType: 'html',
-  platforms: ['web', 'weex', 'wx'],
+  // builtinNpmName: 'cml-tt-ui-builtin',
+  extPlatform: {
+    tt: 'cml-tt-plugin',
+  },
+  babelPath: [
+    path.join(__dirname,'node_modules/cml-tt-ui-builtin'),
+    path.join(__dirname,'node_modules/cml-tt-runtime'),
+    path.join(__dirname,'node_modules/cml-tt-api'),
+    path.join(__dirname,'node_modules/cml-tt-ui'),
+    path.join(__dirname,'node_modules/cml-tt-store'),
+    path.join(__dirname,'node_modules/cml-tt-mixins'),
+    path.join(__dirname,'node_modules/mobx'),
+  ],
+  baseStyle:{
+    wx: true,
+    web: true,
+    weex: true,
+    alipay: true,
+    baidu: true,
+    qq: true,
+    tt:true,
+  },
+  platforms: ['web', 'weex', 'wx','tt'],
   buildInfo: {
     wxAppId: '123456',
     wxEntryPage: '',
